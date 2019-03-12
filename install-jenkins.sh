fcdaa3d524325959ca724e09174b154d66c6204f
mateuszo <mateuszo@shockerlizard.local>
2019-03-12 11:31:49 +0100

    ...

---
+++ install-jenkins.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+yum install java-1.8.0-openjdk-devel -y
+curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
+rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+yum install jenkins -y
+systemctl enable jenkins
+systemctl start jenkins
